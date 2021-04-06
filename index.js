657e508fdf40ee2489fbc8a508006abf79c25340
DanielJang <daniel.jang.0621@gmail.com>
2021-04-07 03:06:02 +0900

    fixed cors error

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+});
+
 app.use("/users", userRouter);
 
 app.listen(port, () => {
