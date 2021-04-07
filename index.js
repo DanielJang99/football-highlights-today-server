5bbedf254c873304e3856c54117d6362ebdedb9c
DanielJang <daniel.jang.0621@gmail.com>
2021-04-07 10:51:09 +0900

    fixed cors error

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 const express = require("express");
-const path = require("path");
 const port = process.env.PORT;
-const dev = process.env.NODE_ENV !== "production";
 require("./db/mongoose");
 const userRouter = require("./routers/user");
-const auth = require("./middleware/auth");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    next();
-});
-
+app.use(cors());
 app.use("/users", userRouter);
 
 app.listen(port, () => {
